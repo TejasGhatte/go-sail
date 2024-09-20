1e509526a98fb23bad491fdb0efd58db53334903
TejasGhate <tejasghatte5@gmail.com>
2024-09-20 21:56:50 +0530

    feat: populate directory

--- internal/scripts/create.go
+++ internal/scripts/create.go
@@ -2,7 +2,10 @@ package scripts
 
 import (
 	"fmt"
+
+	"github.com/TejasGhatte/go-sail/internal/models"
 	"github.com/TejasGhatte/go-sail/internal/prompts"
+	"github.com/TejasGhatte/go-sail/internal/configs"
 )
 
 func CreateProject(name string) {
@@ -18,4 +21,26 @@ func CreateProject(name string) {
 
 	fmt.Println("Generating project with the following options:")
     fmt.Printf("Framework: %s, Database: %s, ORM: %s, Logging: %t, Caching: %t\n", framework, database, orm, logging, caching)
+	
+	ctx := models.Options{
+		ProjectName: name,
+		Framework:   framework,
+		Database:    database,
+		ORM:         orm,
+		Caching:     caching,
+		Logging:     logging,
+	}
+
+	err := PopulateDirectory(ctx)
+	if err != nil {
+		fmt.Println("Error creating project:", err)
+	}
+}
+
+func PopulateDirectory(ctx models.Options) error {
+
+	if err := GitClone(ctx.ProjectName, ctx.Framework, configs.FIBER_URL); err != nil {
+		return fmt.Errorf("error cloning repository: %v", err)
+	}
+	return nil
 }
